ea2143b2937e9cfac0696ed8775a4d989e19f495
Taeil-Nam <tnam@student.42seoul.kr>
2023-08-23 19:16:08 +0900

    Feat: bvh 트리 생성
    - bvh 트리 생성용 재귀 함수 추가.
    - Makefile 수정.
    - random_int(), random_int_mm() 함수 추가.

--- src/bvh/bvh.c
+++ src/bvh/bvh.c
@@ -2,26 +2,49 @@
 
 int	random_int_mm(int min, int max);
 
-t_hittable	*make_bvh(t_list *list)
+t_comparator	set_comparator(void)
 {
-	t_hittable		**hittables;
-	t_hittable		*root;
-	t_comparator	comparator;
-	int				random_num;
+	int	random_num;
 
-	root = (t_hittable *)malloc(sizeof(t_hittable));
-	hittables = list_to_hittable_arr(list);
 	random_num = random_int_mm(0, 2);
 	if (random_num == 0)
-		comparator = compare_x;
+		return (compare_x);
 	else if (random_num == 1)
-		comparator = compare_y;
-	else if (random_num == 2)
-		comparator = compare_z;
-	quick_sort(hittables, 0, ft_lstsize(list), comparator);
-	// root->hit = aabb_hit;
-	root->left = make_child_node();
-	root->right = make_child_node();
-	//root->b_box = aabb_b_box; left, right의 b_box를 사용하여 구해야함 확인 필요.
-	return (root);
+		return (compare_y);
+	return (compare_z);
+}
+
+t_aabb	*surrounding_box(t_aabb box1, t_aabb box2)
+{
+	t_aabb	*new_box;
+
+	new_box = (t_aabb *)malloc(sizeof(t_aabb));
+	new_box->p_min.x = fmin(box1.p_min.x, box2.p_min.x);
+	new_box->p_min.y = fmin(box1.p_min.y, box2.p_min.y);
+	new_box->p_min.z = fmin(box1.p_min.z, box2.p_min.z);
+	new_box->p_max.x = fmax(box1.p_max.x, box2.p_max.x);
+	new_box->p_max.y = fmax(box1.p_max.y, box2.p_max.y);
+	new_box->p_max.z = fmax(box1.p_max.z, box2.p_max.z);
+	return (new_box);
+}
+
+t_hittable	*make_bvh(t_hittable **hittables, int start, int end)
+{
+	t_hittable		*node;
+	t_comparator	comparator;
+	int				mid;
+
+	if (start == end)
+		return (hittables[start]);
+	node = (t_hittable *)malloc(sizeof(t_hittable));
+	comparator = set_comparator();
+	quick_sort(hittables, start, end, comparator);
+	mid = (start + end) / 2;
+	// node->hit = aabb_hit;
+	// node->b_box = aabb_b_box;
+	node->left = make_bvh(hittables, start, mid);
+	node->right = make_bvh(hittables, mid, end);
+	node->object = surrounding_box(node->left->b_box(node->left->object),
+			node->right->b_box(node->right->object));
+	return (node);
 }
--- src/main.c
+++ src/main.c
@@ -148,11 +148,14 @@ int	main(int argc, char *argv[])
 	t_list		*list;
 	t_camera	cam;
 	t_hittable	*bvh;
+	t_hittable	**hittables;
 
 	list = NULL;
 	if (argc != 2)
 		return (0);
 	minirt_parser(argv[1], &list, &cam);
-	bvh = make_bvh(list);
+	hittables = list_to_hittable_arr(list);
+	bvh = make_bvh(hittables, 0, ft_lstsize(list) - 1);
+	ft_lstclear(&list, dummy);
 	return (0);
 }
